d5bf88363852aa17e43ba4fbc51f0a88e35ebf94
Tommy Berman <tomy.berman1@gmail.com>
2019-12-06 22:21:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,6 @@ require 'pp'
 
 def reverse_each_word(str)
   str_arr = str.split(" ")
-  rev = ""
   
   # str_arr.each_with_index { |word, wordIndex| 
   #   rev += word.reverse
@@ -10,6 +9,8 @@ def reverse_each_word(str)
   #     rev += " "
   #   end
   # } 
+  
+  rev = str_arr.collect { |word| word.reverse }
   
   rev
 end
