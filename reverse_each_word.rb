6689e69b59a93f4db37d091bee04a8a17f8d4bbf
Tommy Berman <tomy.berman1@gmail.com>
2019-12-06 22:18:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,12 +4,12 @@ def reverse_each_word(str)
   str_arr = str.split(" ")
   rev = ""
   
-  str_arr.each_with_index { |word, wordIndex| 
-    rev += word.reverse
-    if wordIndex < str_arr.length-1
-      rev += " "
-    end
-  } 
+  # str_arr.each_with_index { |word, wordIndex| 
+  #   rev += word.reverse
+  #   if wordIndex < str_arr.length-1
+  #     rev += " "
+  #   end
+  # } 
   
   rev
 end
